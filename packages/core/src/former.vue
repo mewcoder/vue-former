c165d5a07b5f4cfabbdbfadc8612b68cccb7eae6
mewcoder <1010953107@qq.com>
2022-11-18 14:39:15 +0800

    feat: add value

--- packages/core/src/former.vue
+++ packages/core/src/former.vue
@@ -1,17 +1,28 @@
 <script setup>
-import { computed, reactive } from 'vue';
+import { ref, computed, reactive, watch } from 'vue';
 import FormerItem from './former-item.vue';
+import { cloneDeep } from 'lodash-es';
 
 const props = defineProps({
   schema: {
     type: Array,
     default: () => []
   },
+  config: {
+    type: Object,
+    default: () => ({ labelPosition: 'top' })
+  },
+  rules: {
+    type: Object,
+    default: () => ({})
+  },
   prefix: {
     type: String,
     default: 'el'
   }
 });
+
+const formRef = ref();
 
 const formKey = props.prefix + '-form';
 const formItemKey = props.prefix + '-form-item';
@@ -21,43 +32,80 @@ const renderList = computed(() => {
 });
 
 const formData = reactive({});
+watch(
+  () => props.schema,
+  (val) => {
+    if (!val || !val.length) return;
+    const list = cloneDeep(val);
+    initFormData(list);
+  },
+  { deep: true, immediate: true }
+);
+
+function initFormData(list) {
+  const data = {};
+  for (const item of list) {
+    initItemValue(item, data);
+  }
+}
+
+function initItemValue(item) {
+  const { model, value } = item;
+  formData[model] = value;
+}
+
+//-----------------------------API-----------------------------
+function getForm() {
+  // 获取页面操作的字段信息
+  const form = cloneDeep(formData);
+  return form;
+}
+
+function resetFields() {
+  formRef.value.resetFields();
+}
+
+defineExpose({ getForm, resetFields });
 </script>
 
 <template>
   <!-- <form> -->
   <component
+    ref="formRef"
     :is="formKey"
-    :modal="formData"
-    label-position="top"
+    :model="formData"
+    v-bind="config"
     @submit.prevent
   >
-    <!-- <form-item> -->
-    <component
-      :is="formItemKey"
-      v-for="item in renderList"
-      :key="item.model"
-      :prop="item.model"
-      :label="item.label || '--'"
-      :description="item.description || '--'"
-    >
-      <!-- content -->
+    <template v-for="(item, i) in renderList" :key="item.model + i">
+      <!-- <form-item> -->
       <component
-        :is="item.type"
-        :ref="'filed-' + item.model"
-        v-model="formData[item.model]"
-        v-bind="item.props"
+        :is="formItemKey"
+        :prop="item.model"
+        :label="item.label || ''"
+        :description="item.description || ''"
+        v-bind="item.config || {}"
       >
-        <!-- slots -->
-        <template v-for="slot in item.slots" :key="slot.name" #[slot.name]>
-          <FormerItem
-            v-if="slot.children"
-            :key="slot.name"
-            :schema="slot.children"
-          />
-        </template>
-        <!-- children -->
-        <FormerItem v-if="item.children" :schema="item.children" />
+        <!-- component -->
+        <component
+          :is="item.type"
+          :key="item.model + item.type"
+          :ref="'filed-' + item.model"
+          v-model="formData[item.model]"
+          v-bind="item.props"
+        >
+          <!-- slots -->
+          <template v-for="slot in item.slots" :key="slot.name" #[slot.name]>
+            <FormerItem
+              v-if="slot.children"
+              :key="slot.name"
+              :schema="slot.children"
+            />
+          </template>
+          <!-- children -->
+          <FormerItem v-if="item.children" :schema="item.children" />
+        </component>
       </component>
-    </component>
+    </template>
   </component>
 </template>
--- packages/playground/src/views/HomeView.vue
+++ packages/playground/src/views/HomeView.vue
@@ -1,15 +1,30 @@
 <script setup>
+import { ref } from 'vue';
 import demo from '../mock/demo.json';
 
 const json = demo;
 
 const prefix = 'El';
+
+const formRef = ref();
+
+function getForm() {
+  const data = formRef.value.getForm();
+  alert(JSON.stringify(data, null, 2));
+}
+function resetForm() {
+  formRef.value.resetFields();
+}
 </script>
 
 <template>
   <div class="home">
     <pre class="pre">{{ JSON.stringify(json, null, 2) }}</pre>
-    <VFormer class="form" :schema="json" :prefix="prefix" />
+    <div>
+      <VFormer ref="formRef" class="form" :schema="json" :prefix="prefix" />
+      <el-button type="primary" @click="getForm()">Submit</el-button>
+      <el-button @click="resetForm()">Reset</el-button>
+    </div>
   </div>
 </template>
 
